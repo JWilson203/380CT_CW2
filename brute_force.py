37a16159d12ee3f55fc99e5ba86494a9d4e7b63b
Luke Smy <smywalker@users.noreply.github.com>
2017-01-06 14:34:20 +0000

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,11 +1,14 @@
 import itertools
 import time
 import random
-     
+
 def brute_force(mylist, target):
     #get all 2 digit subsets of the list
-    results =(list(itertools.combinations(mylist, 2)))
+    results =[]
     total = 0
+    for j in range(0,len(mylist)+1):
+        results += itertools.combinations(mylist,j)
+
     for set in results:
         #set total to the sum of the current set
         total = sum(set)
@@ -18,9 +21,9 @@ def brute_force(mylist, target):
             print (" not a match")
     print("no match was found!!!!")
     return
-#generate random list and target
-mylist = random.sample(range(10), 8)
-target = random.randint(1,10)
+#generate random list and target, change these for different sizes
+mylist = random.sample(range(30), 8)
+target = random.randint(1,30)
 
 #call brute force and time it
 start_time = time.time()
